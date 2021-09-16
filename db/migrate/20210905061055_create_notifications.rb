d603a4559add6f418e18355b3cfc66dae7c76e6e
hiraitomoyuki <kotemo0605@gmail.com>
2021-09-16 03:15:55 +0000

    マイグレーションファイル変更

--- db/migrate/20210905061055_create_notifications.rb
+++ db/migrate/20210905061055_create_notifications.rb
@@ -3,7 +3,6 @@ class CreateNotifications < ActiveRecord::Migration[5.2]
     create_table :notifications do |t|
       t.integer :visitor_id
       t.integer :visited_id
-
       t.integer :article_id
       t.integer :comment_id
       t.integer :schedule_id
--- db/migrate/20210912154220_remove_event_id_from_entries.rb
+++
@@ -1,5 +0,0 @@
-class RemoveEventIdFromEntries < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :entries, :event_id, :integer
-  end
-end
