4717791ce60ce83fd3c7ac11aebe9f76da5a795c
hiraitomoyuki <kotemo0605@gmail.com>
2021-09-16 01:05:18 +0000

    マイグレーションファイル変更

--- db/migrate/20210908104311_remove_circlue_visited_id_from_notifications.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCirclueVisitedIdFromNotifications < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :notifications, :circlue_visited_id, :integer
-  end
-end
--- db/migrate/20210908104337_remove_circlue_visitor_id_from_notifications.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCirclueVisitorIdFromNotifications < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :notifications, :circlue_visitor_id, :integer
-  end
-end
