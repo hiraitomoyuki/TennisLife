844b9516bf13bbd60cd986ff1607c13e03d7b619
hiraitomoyuki <kotemo0605@gmail.com>
2021-09-22 08:10:23 +0000

    管理者機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_21_043203) do
+ActiveRecord::Schema.define(version: 2021_09_22_040011) do
 
   create_table "approvals", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -216,6 +216,7 @@ ActiveRecord::Schema.define(version: 2021_09_21_043203) do
     t.string "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
